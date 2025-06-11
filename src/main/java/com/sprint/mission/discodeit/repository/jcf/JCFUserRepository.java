e35d1780019698c53fccdd189099cbb0967a73a6
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2025-06-11 15:04:19 +0900

    feat : implement JCFUserRepository

---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFUserRepository.java
@@ -0,0 +1,47 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class JCFUserRepository implements UserRepository {
+
+    private final List<User> data;
+
+    private static JCFUserRepository jcfUserRepository;
+
+    public static synchronized JCFUserRepository getInstance() {
+        if (jcfUserRepository == null) {
+            jcfUserRepository = new JCFUserRepository();
+        }
+        return jcfUserRepository;
+    }
+
+    private JCFUserRepository() {
+        this.data = new ArrayList<>();
+    }
+
+    @Override
+    public void save(User user) {
+        data.add(user);
+    }
+
+    @Override
+    public List<User> findUsers() { return data; }
+
+    @Override
+    public Optional<User> findUserById(UUID userId) {
+        return data.stream()
+                .filter(user -> user.getId() == userId)
+                .findFirst();
+    }
+
+    @Override
+    public void delete(User user) {
+        data.remove(user);
+    }
+
+}
