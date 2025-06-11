2f6e91c4fa5fe37da64dfe60521031560f770680
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2025-06-11 15:24:09 +0900

    feat : implement JCFChannelRepository and JCFMessageRepository

---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
@@ -0,0 +1,48 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class JCFMessageRepository implements MessageRepository {
+
+    private final List<Message> data;
+    private static JCFMessageRepository jcfMessageRepository;
+
+    public static synchronized JCFMessageRepository getInstance() {
+        if(jcfMessageRepository == null) {
+            jcfMessageRepository = new JCFMessageRepository();
+        }
+        return jcfMessageRepository;
+    }
+
+    private JCFMessageRepository() {
+        this.data = new ArrayList<>();
+    }
+
+    @Override
+    public void save(Message message) {
+        data.add(message);
+    }
+
+    @Override
+    public Optional<Message> findMessageById(UUID messageId) {
+        return data.stream()
+                .filter(message -> message.getId() == messageId)
+                .findFirst();
+    }
+
+    @Override
+    public List<Message> findMessages() {
+        return data;
+    }
+
+    @Override
+    public void delete(Message message) {
+        data.remove(message);
+    }
+
+}
