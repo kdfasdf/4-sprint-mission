0d2647bae0287b744c36c47f5c6139f8168b3431
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2025-07-01 16:46:52 +0900

    feat : add binaryContentController

---
+++ src/main/java/com/sprint/mission/discodeit/api/controller/BinaryContentController.java
@@ -0,0 +1,25 @@
+package com.sprint.mission.discodeit.api.controller;
+
+import com.sprint.mission.discodeit.dto.binarycontent.BinaryContentResponse;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class BinaryContentController {
+
+    private final BinaryContentService binaryContentService;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/binarycontents")
+    public ResponseEntity<List<BinaryContentResponse>> getBinaryContents(@RequestBody List<UUID> filesId) {
+        return ResponseEntity.ok(binaryContentService.findAllByIdIn(filesId));
+    }
+}
