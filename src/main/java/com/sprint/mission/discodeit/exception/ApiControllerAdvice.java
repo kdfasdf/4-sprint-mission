97549b289c19fdf25bab6f36f0fa91255fbb7218
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2025-07-07 16:09:01 +0900

    feat : add exception handler method

--- src/main/java/com/sprint/mission/discodeit/exception/ApiControllerAdvice.java
+++ src/main/java/com/sprint/mission/discodeit/exception/ApiControllerAdvice.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -21,6 +23,26 @@ public class ApiControllerAdvice extends ResponseEntityExceptionHandler {
                 .body(ApiResponse.onFailure(ex.getMessage()));
     }
 
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ApiResponse<?>> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(ApiResponse.onFailure(e.getMessage()));
+    }
+
+    /**
+     * @ModelAttribute 로 binding error 발생 시
+     * 바인딩 필드 타입 불일치, 유효성 검사 길패
+     */
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ApiResponse<?>> handleBindException(BindException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.onFailure(e.getMessage()));
+    }
+
+    /**
+     * 요청 파라미터가 컨트롤러 메서드의 파라미터 타입과 맞지 않을 때
+     * String -> UUID로 받으려고 하거나 id=abc를 Long으로 받으려고 하거나
+     */
     @Override
     protected ResponseEntity<Object> handleTypeMismatch(
             TypeMismatchException ex,
@@ -34,6 +56,9 @@ public class ApiControllerAdvice extends ResponseEntityExceptionHandler {
     }
 
 
+    /**
+     * JSON 파싱 에러 등
+     */
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
                                                                HttpHeaders headers, HttpStatusCode status,
